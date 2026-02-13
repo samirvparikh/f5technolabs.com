421255bb5f7d805ac71f038dd2987234256ce617
SAMIR PARIKH <51854332+samirvparikh@users.noreply.github.com>
2026-02-13 11:03:22 +0530

    Remove SMTP configuration from config.php

--- api/config.php
+++ api/config.php
@@ -3,10 +3,4 @@
  * Mail configuration - ZeptoMail SMTP (info@rfpplanet.com)
  * Uses getenv() if set, otherwise defaults below.
  */
-// Use smtp.zeptomail.com or smtp.zeptomail.in (India)
-define('MAIL_HOST', 'smtp.zeptomail.in');
-define('MAIL_PORT', 587);
-define('MAIL_USERNAME', 'info@rfpplanet.com');
-define('MAIL_PASSWORD', 'PHtE6r0IFOnt3TJ59BEBt/TuEZSgPYx7+L9vLQgWs9pAAvIAF00HqNh5wGOz/U8qB6JAE/SZzNk9s7LP5b+NcG/oMDlFWmqyqK3sx/VYSPOZsbq6x00Ytlocf0PbU4TscNVv3S3SuN7bNA==');
-define('MAIL_FROM_ADDRESS', 'info@rfpplanet.com');
-define('MAIL_FROM_NAME', 'rfpplanet');
+
